b8f20af5dfafd92c362651b82ba913d1dfeac0a3
haiderali <mail.haiderali94@gmail.com>
2023-06-22 15:08:52 +0500

    added projects section and hydrated data

--- components/user/descriptions.js
+++ components/user/descriptions.js
@@ -1,13 +1,20 @@
 import Experience from "./experiencedata/experience";
+import Projects from "./projects/projects";
 
 const PersonalInfo = ({ data, about, experience, projects }) => {
   return (
-    <div className={`${experience ? 'h-1/2 overflow-y-scroll' : ''}`} >
+    <div className={`${experience || projects ? 'h-1/2 overflow-y-scroll' : ''}`} >
       {about && <p className="text-md font-thin leading-6 ml-[120px]">{data.about}</p>}
       {experience &&
         data.experience.map((exp) => (
           <div key={exp.id} className="grid grid-cols-[120px,1fr] items-start">
             <Experience experience={exp} />
+          </div>
+        ))}
+      {projects &&
+        data.projects.map((pro) => (
+          <div key={pro.id} className="grid grid-cols-[1fr] items-start">
+            <Projects projects={pro} />
           </div>
         ))}
     </div>
