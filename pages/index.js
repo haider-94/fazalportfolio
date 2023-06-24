9e3834a1f0d64ab1c7c687e07c41e5fcaa7c0db0
haiderali <mail.haiderali94@gmail.com>
2023-06-24 20:37:49 +0500

    added seo

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Portfolio from "@/components/container/portfolio";
 import { Poppins } from "next/font/google";
+import Head from "next/head";
 
 const inter = Poppins({
   weight: ["100", "200", "300", "400", "500", "700", "800", "900"],
@@ -8,8 +9,14 @@ const inter = Poppins({
 
 export default function Home() {
   return (
-    <main className={`${inter.className}`}>
-      <Portfolio />
-    </main>
+    <>
+    <Head>
+      <title>Fazal Karim</title>
+      <meta name="description" content="Fazal Karim Portfolio"/>
+    </Head>
+      <main className={`${inter.className}`}>
+        <Portfolio />
+      </main>
+    </>
   );
 }
